91bae46e801d2a06afe2ad92a86ab811ac54437e
Doit <duhee7524@naver.com>
2024-02-02 16:47:55 +0900

    feat: 공용 카드 컴포넌트 구현
    feat: 공용 카드 컴포넌트 구현

---
+++ src/store/slices/authSlices.ts
@@ -0,0 +1,23 @@
+import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface AuthState {
+  isAuthenticated: boolean;
+}
+
+const initialAuthState: AuthState = { isAuthenticated: false };
+
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export const authActions = authSlice.actions;
+export default authSlice.reducer;
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,8 +1,10 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import darkModeSlices from "./slices/darkModeSlices"; // 실제 경로에 맞게 수정
+import authSlices from "./slices/authSlices";
 
 const rootReducer = combineReducers({
   darkMode: darkModeSlices,
+  auth:authSlices,
   // NOTE: 다른 리듀서 추가할 것 
 });
 
@@ -11,6 +13,7 @@ export type RootState = ReturnType<typeof rootReducer>;
 const store = configureStore({
   reducer: {
     darkMode: darkModeSlices,
+    auth:authSlices,
   },
 });
 
