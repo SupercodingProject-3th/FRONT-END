2d117bcf64c042e524dbf75de963842868158e1c
MangwonCassie <dudnfsla1@naver.com>
2024-02-04 02:24:56 +0900

    feat: Header 메인페이지 라우팅 구현

--- src/shared/Header.tsx
+++ src/shared/Header.tsx
@@ -1,82 +1,83 @@
-    import styled from "styled-components";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
+import LogoImage from "../assets/icon/logo.png";
+import SearchBar from "../components/mainPageLayout/SearchBar";
+import DarkmodeBtn from "../components/mainPageLayout/DarkModeToggleButton";
+import UserMenu from "../components/mainPageLayout/UserMenu";
 
-    import LogoImage from "../assets/icon/logo.png";
-    import SearchBar from "../components/mainPageLayout/SearchBar";
-    import DarkmodeBtn from "../components/mainPageLayout/DarkModeToggleButton";
-    import UserMenu from "../components/mainPageLayout/UserMenu";
+const Header = () => {
+  return (
+    <HeaderLayout>
+      <HeaderContainer>
+        <TopNavBar>
+          <LogoWrapper>
+            <Link to="/">
+              <Logo src={LogoImage} alt="Logo"></Logo>
+            </Link>
+          </LogoWrapper>
+          <SearchBar />
+          <ButtonsWrapper>
+            <DarkmodeBtn isMobile={false} />
+            <UserMenu />
+          </ButtonsWrapper>
+        </TopNavBar>
+      </HeaderContainer>
+      <UnderLine />
+    </HeaderLayout>
+  );
+};
 
+export default Header;
 
-    const Header = () => {
-      return (
-        <HeaderLayout>
-          <HeaderContainer>
-            <TopNavBar>
-              <LogoWrapper>
-                <Logo src={LogoImage}  alt="Logo"/>
-              </LogoWrapper>
-              <SearchBar />
-              <ButtonsWrapper>
-                <DarkmodeBtn isMobile={false} />
-                <UserMenu />
-              </ButtonsWrapper>
-            </TopNavBar>
-          </HeaderContainer>
-          <UnderLine />
-        </HeaderLayout>
-      );
-    };
+const HeaderLayout = styled.div`
+  width: 100%;
+  background-color: #fff;
+  z-index: 9990;
+  font-size: 16px;
+  transition: transform 0.3s ease-in-out;
+  z-index: 1000;
+`;
 
-    export default Header;
+const HeaderContainer = styled.div`
+  padding: 0 2rem;
+  width: 100%;
+  max-width: 120rem;
+  margin: 0 auto;
 
-    const HeaderLayout = styled.div`
-      width: 100%;
-      background-color: #fff;
-      z-index: 9990;
-      font-size: 16px;
-      transition: transform 0.3s ease-in-out;
-      z-index: 1000; 
-    `;
+  //NOTE: 추가했음 그 검색창 왼쪽으로 땡기려고 카테고리들어오면 수정해야함
+  position: relative;
+`;
 
-    const HeaderContainer = styled.div`
-      padding: 0 2rem;
-      width: 100%;
-      max-width: 120rem;
-      margin: 0 auto;
+const TopNavBar = styled.div`
+  position: relative;
+  display: flex;
+  height: 7rem;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1.2rem;
 
-      //NOTE: 추가했음 그 검색창 왼쪽으로 땡기려고 카테고리들어오면 수정해야함
-      position: relative;
-    `;
+  @media (min-width: 1024px) {
+    height: 8rem;
+  }
+`;
 
-    const TopNavBar = styled.div`
-      position: relative;
-      display: flex;
-      height: 7rem;
-      width: 100%;
-      align-items: center;
-      justify-content: space-between;
-      gap: 1.2rem;
+const UnderLine = styled.div`
+  display: block;
+  border-bottom: 0.1rem solid rgb(200, 200, 200);
+`;
 
-      @media (min-width: 1024px) {
-        height: 8rem;
-      }
-    `;
+const LogoWrapper = styled.div`
+  @media (max-width: 1023.9px) {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+`;
 
-    const UnderLine = styled.div`
-      display: block;
-      border-bottom: 0.1rem solid rgb(200, 200, 200);
-    `;
-
-    const LogoWrapper = styled.div`
-      @media (max-width: 1023.9px) {
-        position: fixed;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%, -50%);
-      }
-    `;
-
-    const ButtonsWrapper = styled.div`
+const ButtonsWrapper = styled.div`
       display: flex;
       gap: 3rem;
       align-items: center;
@@ -89,10 +90,7 @@
       }
     `;
 
-
-    const Logo = styled.img`
-    width: 100px; 
-    height: 100px; //NOTE: 가로 길이에 맞추어 세로 길이를 조절
-
-  `;
-
+const Logo = styled.img`
+  width: 100px;
+  height: 100px; //NOTE: 가로 길이에 맞추어 세로 길이를 조절
+`;
