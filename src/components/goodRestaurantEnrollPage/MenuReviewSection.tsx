1ed15369ea496b0318b38cc57409476c14217942
MangwonCassie <dudnfsla1@naver.com>
2024-02-11 01:06:38 +0900

    feat: 맛집등록페이지 별 ui만 완성

--- src/components/goodRestaurantEnrollPage/MenuReviewSection.tsx
+++ src/components/goodRestaurantEnrollPage/MenuReviewSection.tsx
@@ -1,12 +1,48 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
+import StarRating from "./StarRating";
 
-const MenuReviewSection: React.FC = () => {
+import filledStar from '../../assets/icon/filled-star.svg';
+import halfStar from '../../assets/icon/half-star.svg';
+import outlineStar from '../../assets/icon/outline-star.svg';
+
+interface MenuReviewSectionProps {
+  rating: number | null;
+  onChange: (value: number | null) => void;
+}
+
+
+const MenuReviewSection: React.FC<MenuReviewSectionProps> = ({ rating, onChange }) => {
+  const [overallRating, setOverallRating] = useState<number | null>(0);
+
+  const handleOverallRatingChange = (value: number | null) => {
+    setOverallRating(value);
+  };
+
   return (
     <StyledMenuReviewSection>
-      <h2>메뉴 및 리뷰</h2>
+      <h2>추천메뉴 조합</h2>
       <textarea placeholder="메뉴를 입력하세요" />
-      <textarea placeholder="리뷰를 입력하세요" />
+      <h2>리뷰 총평</h2>
+      <StarRating rating={overallRating || 0} onChange={handleOverallRatingChange} />
+      <StarRatingUI>
+        {/* 별점에 따라 SVG 아이콘을 렌더링 */}
+        {[...Array(5)].map((_, index) => (
+          <StarImage
+            key={index}
+            src={
+              overallRating !== null &&
+              index < Math.floor(overallRating)
+                ? filledStar
+                : overallRating !== null &&
+                  index < overallRating
+                ? halfStar
+                : outlineStar
+            }
+            alt="star"
+          />
+        ))}
+      </StarRatingUI>
     </StyledMenuReviewSection>
   );
 };
@@ -18,4 +54,13 @@ const StyledMenuReviewSection = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 1rem;
-  `
+`;
+
+const StarRatingUI = styled.div`
+  font-size: 24px;
+`;
+
+const StarImage = styled.img`
+  width: 24px;
+  height: 24px;
+`;
